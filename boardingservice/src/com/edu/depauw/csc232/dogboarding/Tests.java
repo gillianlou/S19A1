09ce9436de8a2dd461883be344ad78ef4fcb62ea
gillianlou <47820667+gillianlou@users.noreply.github.com>
2019-04-08 21:59:18 -0400

    Add files via upload

---
+++ boardingservice/src/com/edu/depauw/csc232/dogboarding/Tests.java
@@ -0,0 +1,35 @@
+package com.edu.depauw.csc232.dogboarding;
+
+import static.org.junit.Assert.*;
+import org.junit.Test;
+
+public class Tests {
+    @Test
+    public void testPetCount ()
+    {
+        Dog c = new Dog("Henry", 5, "lab");
+        DogBoarding system = new DogBoarding();
+        Customer a = system.newCustomer (4, "Gillian", "Hughes", "email.gillianhughes_2021@depauw.edu");
+        a.addPet(c);
+        assertEquals(1, a.getDogCount());
+    }
+
+    @Test
+    public void testGetAge ()
+    {
+        Dog izzy = new Dog("Izzy", 2, "german shepard");
+        DogBoarding system = new DogBoarding();
+        Customer summer = system. newCustomer (7, "Summer", "Asad", "summerasad_2020@depauw.edu");
+        summer.addPet(izzy);
+        assertEquals(5, izzy.getAge());
+    }
+
+    @Test
+    public void testGetBreed ()
+    {
+        Dog c = new Dog("Henry",5, "lab");
+        Customer a = new Customer (4, "Gillian", "Hughes", "gillianhughes_2020@depauw.edu");
+        a.addPet(c);
+        assertEquals(lab, a.getBreed());
+    }
+}
